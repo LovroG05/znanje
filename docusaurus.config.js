69df80174464dccc2b27896607a2c0bad290145d
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2022-11-04 12:39:44 +0100

    NPB, RAO

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,129 +3,157 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Znanje",
-	tagline: "Zakaj bi pisal, če lahko bereš.",
-	url: "https://znanje.tk",
-	baseUrl: "/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/logo.svg",
+  title: "Znanje",
+  tagline: "Zakaj bi pisal, če lahko bereš.",
+  url: "https://znanje.tk",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.svg",
 
-	organizationName: "LovroG05",
-	projectName: "Znanje",
+  organizationName: "LovroG05",
+  projectName: "Znanje",
 
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en"],
-	},
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-	presets: [
-		[
-			"classic",
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					path: "testi",
-					routeBasePath: "testi",
-					sidebarPath: require.resolve("./sidebars.js"),
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: "testi",
+          routeBasePath: "testi",
+          sidebarPath: require.resolve("./sidebars.js"),
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
 
-	plugins: [
-		[
-			"@docusaurus/plugin-content-docs",
-			{
-				id: "gimnazija",
-				path: "gimnazija",
-				routeBasePath: "gimnazija",
-				sidebarPath: require.resolve("./sidebars.js"),
-			},
-		],
-	],
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "gimnazija",
+        path: "gimnazija",
+        routeBasePath: "gimnazija",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "racunalnistvo",
+        path: "racunalnistvo",
+        routeBasePath: "racunalnistvo",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+  ],
 
-	themeConfig:
-	/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: "Znanje",
-				logo: {
-					alt: "My Site Logo",
-					src: "img/logo.svg",
-				},
-				items: [
-					{
-						type: "docsVersion",
-						docsPluginId: "gimnazija",
-						position: "left",
-						label: "Gimnazija"
-					},
-					{
-						type: 'doc',
-						docId: 'intro',
-						position: 'left',
-						label: 'Testi',
-					},
-					{
-						href: "https://github.com/LovroG05/znanje",
-						label: "GitHub",
-						position: "right",
-					},
-				],
-			},
-			footer: {
-				style: "dark",
-				links: [
-					{
-						title: "Programi",
-						items: [
-							{
-								label: "Gimnazija",
-								to: "/gimnazija",
-							},
-						],
-					},
-					{
-						title: "Uporabne zadeve",
-						items: [
-							{
-								label: "Bewiesen",
-								href: "https://play.google.com/store/apps/details?id=com.neleks.bewiesen",
-							},
-							{
-								label: "vFinder",
-								href: "https://vfinder.janhar.si",
-							},
-						],
-					},
-					{
-						title: "Kontakt",
-						items: [
-							{
-								label: "Github",
-								href: "https://github.com/LovroG05/znanje",
-							},
-							{
-								label: "@LovroG05",
-								href: "https://perchperkins.ml/",
-							},							{
-								label: "@chocoearly44",
-								href: "https://github.com/chocoearly44",
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} Znanje. Od vegovcov za vas.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-		}),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Znanje",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docsVersion",
+            docsPluginId: "racunalnistvo",
+            position: "left",
+            label: "Računalništvo",
+          },
+          {
+            type: "docsVersion",
+            docsPluginId: "gimnazija",
+            position: "left",
+            label: "Gimnazija",
+          },
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Testi",
+          },
+          {
+            href: "https://github.com/LovroG05/znanje",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Programi",
+            items: [
+              {
+                label: "Računalništvo",
+                to: "/racunalnistvo",
+              },
+              {
+                label: "Gimnazija",
+                to: "/gimnazija",
+              },
+            ],
+          },
+          {
+            title: "Uporabne zadeve",
+            items: [
+              {
+                label: "Bewiesen",
+                href: "https://play.google.com/store/apps/details?id=com.neleks.bewiesen",
+              },
+              {
+                label: "vFinder",
+                href: "https://vfinder.janhar.si",
+              },
+              {
+                label: "Vegova Urniki",
+                href: "https://v.sync.si",
+              },
+            ],
+          },
+          {
+            title: "Kontakt",
+            items: [
+              {
+                label: "Github",
+                href: "https://github.com/LovroG05/znanje",
+              },
+              {
+                label: "@LovroG05",
+                href: "https://perchperkins.ml/",
+              },
+              {
+                label: "@chocoearly44",
+                href: "https://github.com/chocoearly44",
+              },
+              {
+                label: "@gapidobri",
+                href: "https://github.com/gapidobri",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Znanje. Od vegovcov za vas.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
